6f6794133b15298900e860f58af3c1f2a0aa874e
Roberth Strand <me@robstr.dev>
2020-05-27 18:15:44 +0200

    Readme

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "display_name" {
+  value = azuread_service_principal.k8s.display_name
+}
--- servicePrincipal.tf
+++ servicePrincipal.tf
@@ -1,8 +1,8 @@
 ## Create a Service Principal, and the necessary application
 ## docs: https://www.terraform.io/docs/providers/azuread/r/service_principal.html
 resource "random_integer" "appnumber" {
-  min = 1
-  max = 4
+  min = 5
+  max = 20
 }
 resource "azuread_application" "kubeapp" {
   name = "kubeapp${random_integer.appnumber.result}"
@@ -20,8 +20,4 @@ resource "azuread_service_principal_password" "k8s" {
   service_principal_id = azuread_service_principal.k8s.id
   value                = random_password.password.result
   end_date_relative    = "24h"
-}
-
-output "display_name" {
-  value = azuread_service_principal.k8s.display_name
 }
